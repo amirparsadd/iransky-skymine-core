b9ae6f160c804b715e6d50964522899dec2c8ebb
BuildTools <unconfigured@null.spigotmc.org>
2024-03-23 18:06:46 +0430

    Crating Project Files

---
+++ src/main/java/com/amirparsa/skymine/SkyMine.java
@@ -0,0 +1,17 @@
+package com.amirparsa.skymine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyMine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
