b139c5a97e64db55a0af8a198eb141810af36aff
Amirparsa Baghdadi <76398455+amirparsadd@users.noreply.github.com>
2024-08-05 23:14:20 +0330

    code cleanup
    i dont know why i did this (specialy when iransky is ded) but when i saw that pyramid i needed to get rid of it

--- src/main/java/com/amirparsa/skymine/utils/Utils.java
+++ src/main/java/com/amirparsa/skymine/utils/Utils.java
@@ -1,17 +1,19 @@
 package com.amirparsa.skymine.utils;
 
 public class Utils {
+    // Used to convert minecraft item names to friendly names
+    // Example: DIAMOND_AXE: Diamond Axe
     public static String getFriendlyText(String text){
         //Credits To My Bro ChatGpt
         String[] words = text.toLowerCase().split("_");
         StringBuilder result = new StringBuilder();
 
         for (String word : words) {
-            if (!word.isEmpty()) {
-                result.append(word.substring(0, 1).toUpperCase())
-                        .append(word.substring(1))
-                        .append(" ");
-            }
+            if (word.isEmpty()) return
+            
+            result.append(word.substring(0, 1).toUpperCase())
+                    .append(word.substring(1))
+                    .append(" ");
         }
 
         return result.toString().trim();
